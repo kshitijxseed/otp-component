e1750991d1278f5b1b3d409a14dccf2e804cdeaa
kshitijsharma <52919574+kshitijsharma@users.noreply.github.com>
2020-06-02 11:11:06 +0530

    Add files via upload

---
+++ vue-otp-input-develop/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div style="display: flex; flex-direction: row;">
+    <v-otp-input
+      ref="otpInput"
+      input-classes="otp-input"
+      separator="-"
+      :num-inputs="4"
+      :should-auto-focus="true"
+      :is-input-num="true"
+      @on-change="handleOnChange"
+      @on-complete="handleOnComplete"
+    />
+
+    <button @click="handleClearInput()">Clear Input</button>
+  </div>
+</template>
+
+<script>
+import OtpInput from './components';
+
+export default {
+  name: 'App',
+  components: {
+    'v-otp-input': OtpInput,
+  },
+  methods: {
+    handleOnComplete(value) {
+      console.log('OTP completed: ', value);
+    },
+    handleOnChange(value) {
+      console.log('OTP changed: ', value);
+    },
+    handleClearInput() {
+      this.$refs.otpInput.clearInput();
+    },
+  },
+};
+</script>
+
+<style lang="less">
+  .otp-input {
+    width: 40px;
+    height: 40px;
+    padding: 5px;
+    margin: 0 10px;
+    font-size: 20px;
+    border-radius: 4px;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    text-align: center;
+    &.error {
+      border: 1px solid red !important;
+    }
+  }
+  .otp-input::-webkit-inner-spin-button,
+  .otp-input::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+</style>
